92a6e1d720f5fac77f274d16265b9cc7f2ef2b7a
rchorney <124630689+rchorney@users.noreply.github.com>
2023-12-19 13:25:37 -0500

    Update FinalProject.py

--- FinalProject.py
+++ FinalProject.py
@@ -2,9 +2,60 @@ import streamlit as st
 import pandas as pd
 import pydeck as pdk
 import matplotlib.pyplot as plt
+import plotly.express as px
 
+def Data_Source_Page():
+    def load_data_source_page():
+        # Title of the page
+        st.title("Parking Meters Data Source Information")
 
-def data_Overview_Page:
+        # Markdown for dataset overview
+        st.markdown("""
+            ## Dataset Overview
+            **Title:** Parking Meters  
+            **Type:** Geospatial  
+            **Description:** This dataset includes comprehensive information about parking meters in the City of Boston. It is updated and maintained by the Boston Transportation Department (BTD) Parking Clerk, ensuring accurate and current data for various applications.  
+    
+            ## Accessibility and Usage
+            **Released:** 2015-08-25  
+            **Last Modified:** 2023-10-13  
+            **Publisher:** Boston Maps  
+            **Classification:** Public Record  
+            **License:** Open Data Commons Public Domain Dedication and License (PDDL) - This license allows for free usage, modification, and distribution of the data.  
+
+            ## Contact and Additional Information
+            **Contact Point:** Analytics Team  
+            **Contact Email:** [analyticsteam@boston.gov](mailto:analyticsteam@boston.gov)  
+            **Landing Page:** [Parking Meters Dataset](https://bostonopendata-boston.opendata.arcgis.com/maps/boston::parking-meters)  
+        """)
+
+        # Embedding a video file
+        st.video("C:/Users/reube/Downloads/pexels-james-hamar-12595889 (2160p).mp4")
+    
+        # Additional information about the dataset
+        st.markdown("""
+            ### About the Dataset
+            The "Parking Meters" dataset provides geospatial data on parking meters across Boston, valuable for urban planning, transportation analysis, and economic studies. It reflects the city's efforts in digitizing and making public essential urban data for broader use.
+        """)
+    
+        # Initializing a session state for feedback list
+        if 'feedback_list' not in st.session_state:
+            st.session_state.feedback_list = []
+
+            # Section for user feedback
+            st.title("Feedback")
+            with st.form("feedback_form"):
+                st.text_area("Please provide your feedback:")
+                submit_button = st.form_submit_button("Submit")
+
+                if submit_button:
+                    # Display a success message upon submission
+                    st.success("Thank you for your feedback!")
+
+    # Call the function to display the data source page
+    load_data_source_page()
+
+def data_Overview_Page():
     st.title("Data Overview")
     # Function to load data from a CSV file
     def load_data():
@@ -109,6 +160,7 @@ def data_Overview_Page:
 
 
 def main():
+    
     # Load the data
     df = pd.read_csv("Parking_Meters.csv")
 
